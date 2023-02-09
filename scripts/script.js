6e98d3984b1b888218b8c1ba4afe3300f4e9a555
Леонид Никулин <75582494+LeonidRed@users.noreply.github.com>
2023-02-09 23:10:11 +0300

    fix: Изменены названия переменных, добавлены чистые функции openPopup closePopup

--- scripts/script.js
+++ scripts/script.js
@@ -1,69 +1,3 @@
-// Выбираем кнопки
-const editBtn = document.querySelector('.profile__button-edit')
-const exitBtn = document.querySelectorAll('.popup__button-exit')
-const addBtn = document.querySelector('.profile__button-add')
-const delBtn = document.querySelector('.element__button-del')
-
-// Выбираем модальные окна и input в них
-const popup = document.querySelector('.popup')
-const popupForm = document.querySelector('.popup__form')
-const popupAdd = document.querySelector('.popup-add')
-const popupFormAdd = document.querySelector('.popup__form_add')
-const popupImg = document.querySelector('.popup-image')
-//popup-edit
-const popupInputName = document.querySelector('#input-name')
-const popupInputProf = document.querySelector('#input-prof')
-//popup-add
-const popupInputTitle = document.querySelector('#input-title')
-const popupInputLink = document.querySelector('#input-link')
-//popup-image
-const popupFigureImage = document.querySelector('.popup__figure-image')
-const popupFigureCaption = document.querySelector('.popup__figure-caption')
-
-// Выбираем поля из секции profile
-const profileInfoName = document.querySelector('.profile__info-name')
-const profileInfoProf = document.querySelector('.profile__info-profession')
-
-// Функция и обработчик на открытие и закрытие модальных окон
-function openPopup(e) {
-  if (e.target.classList.contains('profile__button-edit')) {
-    popup.classList.add('popup_opened')
-    popupInputName.value = profileInfoName.textContent
-    popupInputProf.value = profileInfoProf.textContent
-  } else if (e.target.classList.contains('profile__button-add')) {
-    popupAdd.classList.add('popup_opened')
-  }
-}
-
-function closePopup() {
-  popup.classList.remove('popup_opened')
-  popupAdd.classList.remove('popup_opened')
-  popupImg.classList.remove('popup_opened')
-}
-
-// Обработчик на кнопки: редактировать, добавить и закрыть
-// редактировать
-editBtn.addEventListener('click', openPopup)
-// добавить
-addBtn.addEventListener('click', openPopup)
-// закрыть
-for (let i = 0; i < exitBtn.length; ++i) {
-  exitBtn[i].addEventListener('click', closePopup)
-}
-
-//Функция и обработчик на сохранение профиля
-function saveInputPopup(event) {
-  event.preventDefault()
-  profileInfoName.textContent = popupInputName.value
-  profileInfoProf.textContent = popupInputProf.value
-  closePopup()
-}
-
-popupForm.addEventListener('submit', saveInputPopup)
-
-
-// --------------- element-template --------------------
-
 // массив для elements
 const initialCards = [
   {
@@ -90,16 +24,84 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-];
+]
+
+// Выбираем кнопки
+const popupProfileOpenButton = document.querySelector('.profile__button-edit')
+const popupCloseButton = document.querySelectorAll('.popup__button-exit')
+const popupAddCardOpenButton = document.querySelector('.profile__button-add')
+const cardDeleteButton = document.querySelector('.element__button-del')
+
+// Выбираем модальные окна и input в них
+const popupProfile = document.querySelector('.popup-profile')
+const popupProfileForm = document.querySelector('.popup__form-profile')
+const popupAddCard = document.querySelector('.popup-add')
+const popupAddCardForm = document.querySelector('.popup__form_add')
+const popupImg = document.querySelector('.popup-image')
+//popup-edit
+const popupProfileInputName = document.querySelector('#input-name')
+const popupProfileInputProf = document.querySelector('#input-prof')
+//popup-add
+const popupAddCardInputTitle = document.querySelector('#input-title')
+const popupAddCardInputLink = document.querySelector('#input-link')
+//popup-image
+const popupFigureImage = document.querySelector('.popup__figure-image')
+const popupFigureCaption = document.querySelector('.popup__figure-caption')
+
+// Выбираем поля из секции profile
+const profileInfoName = document.querySelector('.profile__info-name')
+const profileInfoProf = document.querySelector('.profile__info-profession')
+
+// Для открытия попапа
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+// Для закрытия попапа
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+// Обработчик на кнопку редактирования профиля
+popupProfileOpenButton.addEventListener('click', openProfilePopup = e => {
+  openPopup(popupProfile)
+  popupProfileInputName.value = profileInfoName.textContent
+  popupProfileInputProf.value = profileInfoProf.textContent
+})
+
+// Обработчик на кнопку добавления новой карточки
+popupAddCardOpenButton.addEventListener('click', openAddCardPopup = e => {
+  openPopup(popupAddCard)
+})
+
+// Обработчик на все кнопки закрытия попапа
+for (let i = 0; i < popupCloseButton.length; ++i) {
+  popupCloseButton[i].addEventListener('click', closeActivePopup = (event) => {
+    closePopup(event.target.parentNode)
+  })
+}
+
+//Функция и обработчик на кнопку сохранение профиля
+function savePopupProfileInput(event) {
+  event.preventDefault()
+  profileInfoName.textContent = popupProfileInputName.value
+  profileInfoProf.textContent = popupProfileInputProf.value
+  closePopup(event.target.parentNode.parentNode)
+}
+
+popupProfileForm.addEventListener('submit', savePopupProfileInput)
+
+
+// --------------- element-template --------------------
 
 // Выбираем шаблон и контейнер для вставки шаблона
 const elementTemplate = document.querySelector('#element-template').content.querySelector('.element')
 const elementsContainer = document.querySelector('.elements')
 
-// Функция отрисовывает каждый element из массива
+// Функция отрисовывает каждый карточку из массива
 function renderElement(arr) {
   const elements = arr.map((item) => { //проходим по каждому элементу массива и возвращаем новый массив HTMLElement
-    return createElement(item)
+    return createCard(item)
   })
   // Добавляем элементы в DOM
   elementsContainer.prepend(...elements)
@@ -107,49 +109,51 @@ function renderElement(arr) {
 
 renderElement(initialCards)
 
-// Функция, которая создает element
-function createElement(el) {
-    elementTemplate.querySelector('.element__picture').src = el.link
-    elementTemplate.querySelector('.element__picture').alt = el.name
-    elementTemplate.querySelector('.element__area-title').textContent = el.name
-    const element = elementTemplate.cloneNode(true)
+// Функция, которая создает карточку
+function createCard(el) {
+  const elementTemplateImg = elementTemplate.querySelector('.element__picture')
+  elementTemplateImg.src = el.link
+  elementTemplateImg.alt = el.name
+  elementTemplate.querySelector('.element__area-title').textContent = el.name
+  const element = elementTemplate.cloneNode(true)
 
-    // добавление лайк-кнопки
-    const likeBtn = element.querySelector('.element__area-like')
-    likeBtn.addEventListener('click', function () {
-      likeBtn.classList.toggle('element__area-like_active')
-    })
+  // добавление лайк-кнопки
+  const likeBtn = element.querySelector('.element__area-like')
+  likeBtn.addEventListener('click', function () {
+    likeBtn.classList.toggle('element__area-like_active')
+  })
 
-    // добавление удаления-кнопки
-    const delBtn = element.querySelector('.element__button-del')
-    delBtn.addEventListener('click', function () {
-      element.remove()
-    })
+  // добавление удаления-кнопки
+  const cardDeleteButton = element.querySelector('.element__button-del')
+  cardDeleteButton.addEventListener('click', function () {
+    element.remove()
+  })
 
-    // добавление попапа с картинкой
-    const elementPic = element.querySelector('.element__picture')
-    elementPic.addEventListener('click', function () {
-      popupImg.classList.add('popup_opened')
-      popupFigureImage.src = el.link
-      popupFigureImage.alt = el.name
-      popupFigureCaption.textContent = el.name
-    })
-    return element
+  // добавление попапа с картинкой
+  const elementPicture = element.querySelector('.element__picture')
+  elementPicture.addEventListener('click', function () {
+    openPopup(popupImg)
+    popupFigureImage.src = el.link
+    popupFigureImage.alt = el.name
+    popupFigureCaption.textContent = el.name
+  })
+
+  return element
 }
 
-// Функция и обработчик на сохранение нового element
-function addNewElement(event) {
+// Функция и обработчик на добавление новой карточки
+function addNewCard(event) {
   event.preventDefault()
 
-  const link = popupInputLink.value
-  const title = popupInputTitle.value
+  const link = popupAddCardInputLink.value
+  const title = popupAddCardInputTitle.value
 
-  const element = createElement({link: link, name: title})
+  const element = createCard({link: link, name: title})
   elementsContainer.prepend(element)
 
-  closePopup()
-  popupInputLink.value = ''
-  popupInputTitle.value = ''
+  closePopup(event.target.parentNode.parentNode)
+  popupAddCardInputLink.value = ''
+  popupAddCardInputTitle.value = ''
 }
 
-popupFormAdd.addEventListener('submit', addNewElement)
+popupAddCardForm.addEventListener('submit', addNewCard)
